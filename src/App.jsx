1ba4ac07e4598c48ed645da960fba77206ba7f6d
Your Name <you@example.com>
2022-12-29 14:50:37 +0100

    Completed the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,27 @@
 import './App.css';
-import Logo from './Imgs/Logo.png'
+import Home from './components/Home';
+import HomeBody from './components/HomeBody';
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
+    // <Router>
+      <div className="App">
       <div className="BG" >
-        <div>
-          <header className=''>
-            <nav className='flex justify-between items-center w-[100%] mx-auto py-10'>
-              <div className=''>
-                <img className='w-20' src={Logo} alt="..." srcset="" />
-              </div>
-              <div className='bg-white md:static absolute md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-20 py-10'>
-                <ul className='flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 text-lowerCase'>
-                  <li>
-                    <a className='hover:text-gray-500' href="wwww"><span className='px-2 font-bold'>00</span>Home</a>
-                  </li>
-                  <li>
-                    <a className='hover:text-gray-500' href="wwww"><span className='px-2 font-bold'>01</span>Destination</a>
-                  </li>
-                  <li>
-                    <a className='hover:text-gray-500' href="wwww"><span className='px-2 font-bold'>02</span>Crew</a>
-                  </li>
-                  <li>
-                    <a className='hover:text-gray-500' href="wwww"><span className='px-2 font-bold'>03</span>Technology</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </header>
-        </div>
 
 
-        <div className='flex justify-around items-center'>
-            <div className='text-white w-96'>
-              <h1 className='text-bold text-9xl'>Space</h1>
-              <p className='text-md pt-8 tracking-widest'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-              Nulla consequuntur doloribus accusamus enim laborum magni reprehenderit fuga deleniti,
-               excepturi sed? Blanditiis quas culpa sit reiciendis eveniet voluptatibus corrupti inventore
-                quibusdam! Cumque at dignissimos consequuntur nihil commodi quo, quis veritatis dolore et 
-                porro minus amet rerum labore obcaecati officiis maiores animi!</p>
-            </div>
+      {/* <Router>
+       <Route path="/"> */}
+        <Home />
+        <HomeBody />
+       {/* </Route>
+      </Router> 
+      <HomeBody /> */}
 
-            <div className='relative bg-white h-80 w-80 rounded-full flex justify-center items-center'>
-              <p className='absolute '>Explore</p>
-            </div>
-        </div>
+      
       </div>
     </div>
+    // {/* </Router> */}
+    
   );
 }
 
---
+++ src/components/Destination.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Destination = () => {
+  return (
+    // <div class="bg-cover bg-center ..." style="background-image: url(...)"></div>
+    <div className='BGDestination'>
+        
+    </div>
+  )
+}
+
+export default Destination
